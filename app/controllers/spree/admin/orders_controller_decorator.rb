1daeeabae1384052636c8befb86093f42967f8c3
Grisha Klimov <klimovgv@gmail.com>
2015-03-27 15:39:09 +0500

    Error message if TMPL is not returning OK on order_cancel, IPCM-187

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -4,7 +4,10 @@ Spree::Admin::OrdersController.class_eval do
   def notify_order_cancelled
     # @order
     api = Marketplace::Api.instance
-    api.cancel_order(@order)
+    if !api.cancel_order(@order)
+      flash[:success] = nil
+      flash[:error] = "Sorry the Order could not be Cancelled at this time. If necessary please speak to the Marketplace Technical Support team to investigate the reason why."
+    end
   end
 
 end
