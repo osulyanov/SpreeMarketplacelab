77967c2c3acca8e647b73b512febb1602ebe6b8f
Grisha Klimov <klimovgv@gmail.com>
2015-06-08 18:00:38 +0500

    Removing ability to sync Products from Spree to TMPL

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -4,14 +4,14 @@ Spree::Admin::ProductsController.class_eval do
 
   def notify_product_created
     # @product
-    api = Marketplace::Api.instance
-    api.create_product(@product)
+    # api = Marketplace::Api.instance
+    # api.create_product(@product)
   end
 
   def notify_product_updated
     # @product
-    api = Marketplace::Api.instance
-    api.update_product(@product)
+    # api = Marketplace::Api.instance
+    # api.update_product(@product)
   end
 
 end
