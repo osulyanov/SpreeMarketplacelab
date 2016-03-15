d392137fb2a8ec5c6d4c57786aba50cf227f4580
yedennek <yedennek@googlemail.com>
2016-03-15 11:45:00 +0000

    IPCM-529: Update cancel method

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -4,10 +4,14 @@ Spree::Admin::OrdersController.class_eval do
   def notify_order_cancelled
     # @order
     api = Marketplace::Api.instance
-    if !api.cancel_order(@order)
+    success, response = api.cancel_ml_order_v2(order, "Cancelled within Spree Admin")
+    if success 
+      Spree::OrderMailer.canceled(order, @seller, "Cancelled within Spree Admin").deliver!
+      flash[:error] = "<strong>Order cancelled!</strong>"
+    else
+      Rails.logger.warn "Order Cancellation Failed - Marketplace error; response: #{response.inspect}"
       flash[:success] = nil
       flash[:error] = "Sorry the Order could not be Cancelled at this time. If necessary please speak to the Marketplace Technical Support team to investigate the reason why."
     end
   end
-
 end
